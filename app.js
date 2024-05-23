7e3c26f5871c2fc3289476bf52d52c4c089915d1
Saeed Noor <163897168+saeednoor786@users.noreply.github.com>
2024-05-23 22:08:21 +0500

    eleven assignment

---
+++ app.js
@@ -0,0 +1,27 @@
+"use strict";
+//Q:11 Names: Store the names of a few of your friends in a array called names. Print each person’s name
+// by accessing each element in the list, one at a time.
+let names = ["ahmed", "salman", "asif", "muskan", "saeed"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
+//Q:12 Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s
+//name, print a message to them. The text of each message should be the same, but each message should be 
+//personalized with the person’s name.
+let Names = ["ahmed", "salman", "asif", "muskan", "saeed"];
+console.log(`wish you all the best, ${names[0]}`);
+console.log(`wish you all the best, ${names[1]}`);
+console.log(`wish you all the best, ${names[2]}`);
+console.log(`wish you all the best, ${names[3]}`);
+console.log(`wish you all the best, ${names[4]}`);
+//Q:13 Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, 
+// and make a list that stores several examples. Use your list to print a series of statements about 
+// these items, such as “I would like to own a Honda motorcycle.”
+let transportation = ["bike", "car", "bus", "railway", "airline"];
+console.log(`I would like to own a, ${transportation[0]}`);
+console.log(`I would like to own a, ${transportation[1]}`);
+console.log(`I would like to own a, ${transportation[2]}`);
+console.log(`I would like to own a, ${transportation[3]}`);
+console.log(`I would like to own a, ${transportation[4]}`);
